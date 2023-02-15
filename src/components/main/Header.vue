852079cf4eb3cdbd41b85555b4dcdaa3ff9af160
Shadin <buhbuhbabah@gmail.com>
2023-02-15 10:04:19 +0400

    add header and first section

---
+++ src/components/main/Header.vue
@@ -0,0 +1,93 @@
+<template lang="pug">
+.header
+  .logo
+    span.a А
+    span тмосфера
+  .box
+    .box-sm.red
+      span Главная
+    .box-sm.orange
+      span Магазин
+    .box-sm.yellow
+      span Контакты 
+    //- .box-sm.green 
+    //- .box-sm.blue 
+    //- .box-sm.purple    
+</template>
+<script>
+export default {
+    name:"HeaderTest"
+
+}
+</script>
+<style scoped>
+.a{
+  position: absolute;
+  border-radius: 100px;
+  border: 2px solid;
+  animation: spin 1.4s linear infinite;
+}
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+.header{
+  position: absolute;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  background: rgb(0, 0, 0, 0.7);
+}
+span:hover{
+  transform: scale(1.1);
+}
+.box {
+  padding: 0 15px 0 0;
+  display: flex;;
+  width: 70%;
+  margin-left:auto;
+}
+
+.box-sm {
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  height: 50px;
+  margin: 0;
+  flex-grow: 1;
+  transition: all .8s ease-in-out;
+  cursor: pointer;
+  border:1px solid #000;
+}
+.box-sm:hover {
+  flex-grow: 2;
+}
+
+.red {
+  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
+  border-radius:10px 0 0 10px
+}
+
+.orange {
+  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
+}
+
+.yellow {
+  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
+  border-radius:0 10px 10px 0
+}
+
+.green {
+  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
+}
+
+.blue {
+  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
+}
+
+.purple {
+  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,22 +1,36 @@
 <template lang="pug">
 .container 
-  .header
-  .box
-    .box-sm.red
-      span Главная
-    .box-sm.orange
-      span Магазин
-    .box-sm.yellow
-      span Контакты 
-    //- .box-sm.green 
-    //- .box-sm.blue 
-    //- .box-sm.purple
-  </div>
+  Header
+  .hero
+    h1 Get scrollin
+    .down
+      svg( viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round")
+      polyline( points="7 13 12 18 17 13")
+      polyline( points="7 6 12 11 17 6")
+    .content 💙
 </template>
 
 <script>
+
+$(window).scroll(function() {
+    console.log("Adasd");
+  var scroll = $(window).scrollTop();
+  $(".hero").css({
+    transform: 'translate3d(0, +'+(scroll/100)+'%, 0) scale('+(100 - scroll/100)/100+')'
+  });
+});
+
+import $ from 'jquery'
+import Header from '../components/main/Header.vue'
+
 export default {
-    
+ name:"MainPage",
+ components: {
+  Header
+ },
+ mounted() {
+  
+ }   
 }
 </script>
 
@@ -28,52 +42,51 @@ export default {
   color: #fff;
 }
 
+@import url('https://rsms.me/inter/inter.css');
 
-.box {
-  display: flex;
-  width: 70%;
-  height: 8px;
-  margin-left:auto;
-}
-
-.box-sm {
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  height: 50px;
+.container {
+  overflow-y: auto;
+  font-family: 'Inter', sans-serif;
+  letter-spacing: -1px;
+  text-align: center;
+  font-size: 20px;
+  height: 100vh;
+  color: #fff;
   margin: 0;
-  flex-grow: 1;
-  transition: all .8s ease-in-out;
-  cursor: pointer;
-  border:1px solid #000;
-}
-.box-sm:hover {
-  flex-grow: 2;
 }
 
-.red {
-  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
-  border-radius:10px 0 0 10px
+.hero {
+  background: url('https://images.unsplash.com/photo-1550353127-b0da3aeaa0ca') no-repeat center center;
+  justify-content: center;
+  background-size: cover;
+  flex-direction: column;
+  border-radius: 10px;
+  align-items: center;
+  position: fixed;
+  display: flex;
+  height: 100vh;
+  width: 100%;
 }
 
-.orange {
-  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
+.down {
+  text-align: center;
+  position: absolute;
+  opacity: 0.6;
+  height: 40px;
+  width: 100%;
+  bottom: 0;
 }
 
-.yellow {
-  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
-  border-radius:0 10px 10px 0
+.content {
+  box-shadow: 0 0 100px rgba(0,0,0,0.4);
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 40px;
+  position: relative;
+  background: #000;
+  height: 2000px;
+  display: flex;
+  top: 100vh;
 }
 
-.green {
-  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
-}
-
-.blue {
-  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
-}
-
-.purple {
-  background: linear-gradient(212deg, rgba(43,43,156,1) 35%, rgba(26,198,233,1) 100%);
-}
 </style>
