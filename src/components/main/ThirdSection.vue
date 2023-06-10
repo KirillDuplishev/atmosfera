5901fcb1310b121cf472c902c07b2cddf6f004a9
Kesha1 <kduplisev@gmail.com>
2023-06-10 22:50:48 +0400

    Третья секция адаптивная

--- src/components/main/ThirdSection.vue
+++ src/components/main/ThirdSection.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   .containerThree
+
     .text-title-section-three
       span.text-40-b(style="color:#452132") Что именно мы делаем
+
     .container-worktype-section-three
       .span-typework-section-three
         WorkType.block-type-section-three( id=1 nameBlock="Поставка и монтаж систем вентиляции" imgSrc="close-up-ventilation-system (2)")
@@ -15,6 +17,17 @@
         WorkType.block-type-section-three( id=7 nameBlock="Поставка и монтаж систем автоматизации вентиляции." imgSrc="close-up-ventilation-system (3)")
         WorkType.block-type-section-three( id=8 nameBlock="Настройка и пусконаладочные работы систем автоматизации, вентиляции кондиционирования. " imgSrc="3d-rendering-ventilation-system")
         WorkType.block-type-section-three( id=9 nameBlock="Реконструкция систем вентиляции и кондиционирования, демонтажные работы." imgSrc="close-up-ventilation-system (7)")
+
+    .container-worktype-section-three-mobile
+        WorkType.block-type-section-three-mobile( id=1 nameBlock="Поставка и монтаж систем вентиляции" imgSrc="close-up-ventilation-system (2)")
+        WorkType.block-type-section-three-mobile( id=2 nameBlock="Поставка и монтаж систем дымоудаления" imgSrc="3d-rendering-ventilation-system (1)")
+        WorkType.block-type-section-three-mobile( id=3 nameBlock="Поставка и монтаж систем кондиционирования" imgSrc="close-up-ventilation-system (1)")
+        WorkType.block-type-section-three-mobile( id=4 nameBlock="Поставка и монтаж воздушного отопления" imgSrc="industrial-vent-coming-out-building")
+        WorkType.block-type-section-three-mobile( id=5 nameBlock="Диагностика и устранение неисправностей существующих систем вентиляций и кондиционирования." imgSrc="close-up-ventilation-system (9)")
+        WorkType.block-type-section-three-mobile( id=6 nameBlock=" Обслуживание систем кондиционирования, кондиционеров." imgSrc="close-up-ventilation-system")
+        WorkType.block-type-section-three-mobile( id=7 nameBlock="Поставка и монтаж систем автоматизации вентиляции." imgSrc="close-up-ventilation-system (3)")
+        WorkType.block-type-section-three-mobile( id=8 nameBlock="Настройка и пусконаладочные работы систем автоматизации, вентиляции кондиционирования. " imgSrc="3d-rendering-ventilation-system")
+        WorkType.block-type-section-three-mobile( id=9 nameBlock="Реконструкция систем вентиляции и кондиционирования, демонтажные работы." imgSrc="close-up-ventilation-system (7)")
 </template>
 
 <script>
@@ -32,34 +45,51 @@ export default {
     width: 50% !important;
     height: auto;
   }
-  /* .block-type-section-three:hover{
-    transform: scale(1);
-  } */
+  .block-type-section-three-mobile {
+    width: 100% !important;
+    height: 205px;
+  }
   .span-typework-section-three {
     height: 25%;
   }
   .containerThree{
-    display: none;
+    background: #D9D9D9 !important;
+    padding-top: 60px !important;
   }
   .containerThreeMobile{
     display: block;
   }
+  .text-title-section-three{
+    margin-bottom: 30px !important;
+  }
   .text-title-section-three span{
     font-size: 30px !important;
+  }
+  .container-worktype-section-three-mobile {
+    display: block !important;
+  }
+  .container-worktype-section-three {
+    display: none;
   }
 }
 .containerThree {
   background: white;
   min-height: 93vh;
+  padding-top: 50px;
+  padding-bottom: 60px;
   height: auto;
   width: 100%;
 }
+.container-worktype-section-three-mobile {
+  display: none;
+  width: 100%;
+  min-height: 90vh;
+  height: auto;
+}
 .text-title-section-three {
   width: 100%;
-  height: 10%;
-  color: rgb(0, 118, 169);
-  padding-top: 20px;
-  margin-bottom: 10px;
+  height: auto;
+  margin-bottom: 50px;
 }
 .container-worktype-section-three {
   width: 100%;
